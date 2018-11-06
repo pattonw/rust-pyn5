989baf2d1da0c5324e306ccff3e63c73884ede0b
pattonw <pattonw@hhmi.org>
2018-11-06 11:28:04 -0500

    Read/Write:
    Added rudimentary write function. Many cases not yet handled.
    
    Added a check for reading from non-existant dataset.

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,15 @@ use pyo3::prelude::*;
 fn read_n5(_py: Python, root_path: &str, path_name: &str, translation: Vec<i64>, dimensions: Vec<i64>) -> PyResult<Vec<u8>> {
     let n = N5Filesystem::open_or_create(root_path).unwrap();
 
+    // return an error rather than panicking to allow program calling read to handle
+    // case of dataset not existing
+    if !n.exists(path_name) {
+        panic!(format!("Dataset {} does not exist!", path_name));
+    }
+
     let bounding_box = BoundingBox::new(translation, dimensions);
     
+    // TODO: rely on dataset attributes.json
     let data_attrs = DatasetAttributes::new(
         vec![10600, 15850, 7062],
         vec![128,128,13],
@@ -27,10 +34,38 @@ fn read_n5(_py: Python, root_path: &str, path_name: &str, translation: Vec<i64>,
     Ok(raw_vec)
 }
 
+#[pyfunction]
+fn write_n5(_py: Python, root_path: &str, path_name: &str, translation: Vec<i64>, dimensions: Vec<i32>, data: Vec<u8>) -> PyResult<()> {
+    let n = N5Filesystem::open_or_create(root_path).unwrap();
+    
+    // TODO: rely on dataset attributes.json if available or allow attributes to be chosen
+    let data_attrs = DatasetAttributes::new(
+        vec![10600, 15850, 7062],
+        vec![128,128,13],
+        DataType::UINT8,
+        CompressionType::new::<compression::gzip::GzipCompression>(),
+    );
+
+    // NOTE: allows writing of blocks that do not allign with designated block size
+    let block_in = VecDataBlock::new(
+        dimensions,
+        translation,
+        data);
+
+    if n.exists(path_name) {
+        n.write_block(path_name, &data_attrs, &block_in)?;
+    } else {
+        n.create_dataset(path_name, &data_attrs)?;
+        n.write_block(path_name, &data_attrs, &block_in)?;
+    } 
+    Ok(())
+}
+
 
 #[pymodinit]
-fn pyn5(py: Python, m: &PyModule) -> PyResult<()> {
+fn libpyn5(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_function!(read_n5))?;
+    m.add_function(wrap_function!(write_n5))?;
 
     Ok(())
 }
