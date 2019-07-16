8f76fa9281ae13857574b3aa02a9b87c6092c125
pattonw <pattonw@hhmi.org>
2019-07-16 14:33:17 -0400

    Use rust version variable in setup_rust script

--- ci/travis/setup_rust.sh
+++ ci/travis/setup_rust.sh
@@ -4,5 +4,5 @@ set -ex
 
 ### Setup Rust toolchain #######################################################
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=nightly
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=$TRAVIS_RUST_VERSION
 export PATH=$PATH:$HOME/.cargo/bin
