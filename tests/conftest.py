951049ae3a88bc8c6e4c7656ca7977c4ecae5432
Chris Barnes <barnesc@janelia.hhmi.org>
2019-07-16 16:32:07 -0400

    Add configurable compression

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,3 +19,14 @@ def ds_dtype(request, tmp_path):
 
     pyn5.create_dataset(n5_path, ds_name, DS_SIZE, BLOCKSIZE, dtype)
     yield pyn5.open(n5_path, ds_name, dtype, False), np.dtype(dtype.lower())
+
+
+@pytest.fixture(params=[
+    {"type": "raw"},
+    {"type": "bzip2", "blockSize": 5},
+    {"type": "gzip", "level": 5},
+    {"type": "lz4", "blockSize": 32768},
+    {"type": "xz", "preset": 3},
+], ids=lambda d: d.get("type", "raw"))
+def compression_dict(request):
+    yield request.param
